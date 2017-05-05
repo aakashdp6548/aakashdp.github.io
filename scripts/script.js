6f78f40f758d4f41039b5cfe6949f1e68efb3066
Aakash Patel <aakashdp@umich.edu>
2017-05-05 01:41:07 -0400

    fixed problems with moveFooter function

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
-var construction = $(".construction");
-var heading = $(".heading");
-var footer = $("#footer");
-
-function moveFooter() {
-    if ($("body").height > $(window).height) {
-        footer.removeClass("bottomOfScreen");
-        footer.addClass("afterElements");
-    }
-    else {
-        footer.removeClass("bottomOfScreen");
-        footer.addClass("afterElements");
-    }
-}
-
 $(document).ready(function(){
+
+    var construction = $(".construction");
+    var heading = $(".heading");
+    var footer = $("#footer");
+
+    function moveFooter() {
+
+        if ($("body").height() > $(window).height()) {
+            footer.removeClass("bottomOfScreen");
+            footer.addClass("afterElements");
+            console.log("scroll" + footer.attr("class"));
+        }
+        else {
+            footer.removeClass("afterElements");
+            footer.addClass("bottomOfScreen");
+            console.log("no scroll" + footer.attr("class"));
+        }
+    }
 
     construction.hide();
     footer.addClass("bottomOfScreen");
