29cf5f250de78cd84433f83a14992698641174f0
Aakash Patel <aakashdp@umich.edu>
2017-06-07 12:37:39 -0400

    formatted background for mobile

--- formcode.php
+++
@@ -1,9 +0,0 @@
-<form method='post'>
-    Name: <input type='text' name='name' id='name' /><br />
-    Email: <input type='text' name='email' id='email' /><br />
-    Comment:<br />
-    <textarea name='comment' id='comment'></textarea><br />
-    <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["id"]; ?>' />
-    <input type='submit' value='Submit' />
-    <input type="reset" value="Reset">
-</form>
